cb35ea8d34db5f4732918dc29979c658bd05e2e4
nnewnew <godsnew2542@gmail.com>
2019-12-18 13:08:50 +0700

    เพิ่ม html id showdata,showid

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6IkF1dGhvcml6YXRpb24iLCJleHAiOjE1NzkyNTI1NDcsInJvbCI6WyJST0xFX1VTRVIiXSwiZCpzaWciOnsicCI6InpXNzB4IiwicyI6bnVsbCwidSI6IjFmYmIxNjNlZDI2MThmZWU2Y2E2YTA1YmYxNjhjZDJhIiwiZiI6InhzeiM5In19.PkFhLNX2_Vmt6ddOI8Xl562-WrE_0x-zUTXyrmBVLxKgTfUjtz9xDAek6Xjrn4CnCvnrcltwpsbqPUT9S5ecwA";
-var data1 = "LA085907385CN";
-// var data2 = "EI486458074TH";
-// var data3 = "RP488749199TH";
-
+var status_dates = [];
+var locations = [];
+var status_descriptions = [];
+var signatures = [];
 $(function() {
   $("#submit").click(function() {
-      
+    var token =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6IkF1dGhvcml6YXRpb24iLCJleHAiOjE1NzkyNTI1NDcsInJvbCI6WyJST0xFX1VTRVIiXSwiZCpzaWciOnsicCI6InpXNzB4IiwicyI6bnVsbCwidSI6IjFmYmIxNjNlZDI2MThmZWU2Y2E2YTA1YmYxNjhjZDJhIiwiZiI6InhzeiM5In19.PkFhLNX2_Vmt6ddOI8Xl562-WrE_0x-zUTXyrmBVLxKgTfUjtz9xDAek6Xjrn4CnCvnrcltwpsbqPUT9S5ecwA";
+    var data1 = "LA085907385CN";
+    // var data2 = "EI486458074TH";
+    // var data3 = "RP488749199TH";
+
     var settings = {
       url: "https://trackapi.thailandpost.co.th/post/api/v1/track",
       method: "POST",
@@ -22,8 +25,38 @@ $(function() {
       })
     };
 
-    $.ajax(settings).done(function(response) {
-      console.log(response);
+    $.ajax(settings).done(function(data) {
+      var testdata = data.response.items.LA085907385CN;
+      for (var i = 0; i < testdata.length; i++) {
+        status_dates.push(testdata[i].status_date);
+        locations.push(testdata[i].location);
+        status_descriptions.push(testdata[i].status_description);
+        signatures.push(testdata[i].signature);
+      }
+
+      $("#showid").empty();
+      for (var i = 0; i < testdata.length; i++) {
+        var id = testdata[i].barcode;
+      }
+      $("#showid").append(id);
+
+      $("#data1").empty();
+      for (var i = 0; i < status_dates.length; i++) {
+        var Status_Dates = status_dates[i] + "<br>";
+        $("#data1").append(Status_Dates);
+      }
+
+      $("#data2").empty();
+      for (var i = 0; i < locations.length; i++) {
+        var Locations = locations[i] + "<br>";
+        $("#data2").append(Locations);
+      }
+
+      $("#data3").empty();
+      for (var i = 0; i < status_descriptions.length; i++) {
+        var Status_Descriptions = status_descriptions[i] + "<br>";
+        $("#data3").append(Status_Descriptions);
+      }
     });
   });
 });
