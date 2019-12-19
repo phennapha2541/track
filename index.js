5ec660e066b70a92221b19be5392b462ccfd9721
nnewnew <godsnew2542@gmail.com>
2019-12-19 12:35:05 +0700

    น่าจะเสร็จละ

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var token =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzZWN1cmUtYXBpIiwiYXVkIjoic2VjdXJlLWFwcCIsInN1YiI6IkF1dGhvcml6YXRpb24iLCJleHAiOjE1NzkyNTI1NDcsInJvbCI6WyJST0xFX1VTRVIiXSwiZCpzaWciOnsicCI6InpXNzB4IiwicyI6bnVsbCwidSI6IjFmYmIxNjNlZDI2MThmZWU2Y2E2YTA1YmYxNjhjZDJhIiwiZiI6InhzeiM5In19.PkFhLNX2_Vmt6ddOI8Xl562-WrE_0x-zUTXyrmBVLxKgTfUjtz9xDAek6Xjrn4CnCvnrcltwpsbqPUT9S5ecwA";
 
 var data1 = "LA085907385CN";
-// var data2 = "EI486458074TH";
-// var data3 = "RP488749199TH";
+var data2 = "EI486458074TH";
 
 var settings = {
   url: "https://trackapi.thailandpost.co.th/post/api/v1/track",
@@ -16,7 +15,7 @@ var settings = {
   data: JSON.stringify({
     status: "all",
     language: "TH",
-    barcode: [data1]
+    barcode: [data1,data2]
   })
 };
 
@@ -24,16 +23,18 @@ var status_dates = [];
 var locations = [];
 var status_descriptions = [];
 var signatures = [];
-//var statu = [];
 
 const Delete_data1 = document.getElementById("data1");
 const Delete_data2 = document.getElementById("data2");
 const Delete_data3 = document.getElementById("data3");
 const Delete_showid = document.getElementById("showid");
+const Delete_img = document.getElementById("Img1");
+
 
 $(function() {
   $("#submit").click(function() {
     var Search = $("#ID").val();
+    var statu;
 
     if (Search == data1) {
       // console.log("True");
@@ -69,11 +70,13 @@ $(function() {
         }
 
         for (var i = 0; i < testdata.length; i++) {
-          var statu = testdata[i].status;
-          // statu.push(testdata[i].status);
-          console.log(statu);
+          var str = testdata[i].status;
+          console.log(str);
 
-          if (statu == 101 || statu == 102 || statu == 103) {
+          statu = str.charAt(0);
+          console.log("char: " + statu);
+
+          if (statu == 1) {
             $("#Img1").empty();
             var img1 = `
           <div class="col mr-2" >
@@ -84,18 +87,8 @@ $(function() {
           </div> 
           `;
             $("#Img1").append(img1);
-          } else if (
-            statu == 101 ||
-            statu == 102 ||
-            statu == 103 && 
-            statu == 201 ||
-            statu == 202 ||
-            statu == 203 ||
-            statu == 204 ||
-            statu == 205 ||
-            statu == 206 ||
-            statu == 207
-          ) {
+          }
+          if (statu == 2) {
             $("#Img1").empty();
             var img2 = `
             <div class="col mr-2" >
@@ -113,20 +106,8 @@ $(function() {
             </center>
           </div>`;
             $("#Img1").append(img2);
-          } else if (
-            statu == 101 ||
-            statu == 102 ||
-            statu == 103 && 
-            statu == 201 ||
-            statu == 202 ||
-            statu == 207 ||
-            statu == 204 ||
-            statu == 205 ||
-            statu == 206 ||
-            statu == 203 &&
-            statu == 301 ||
-            statu == 302
-          ){
+          }
+          if (statu == 3) {
             $("#Img1").empty();
             var img3 = `
             <div class="col mr-2" >
@@ -150,41 +131,46 @@ $(function() {
               <img src="img/showdata/on-delivery.gif" alt="">
               <h6>นำจ่ายพัสดุ</h6>
             </center>
-          </div>`
-          $("#Img1").append(img3);
+          </div>`;
+            $("#Img1").append(img3);
           }
-          else if (
-            statu == 101 ||
-            statu == 102 ||
-            statu == 103 && 
-            statu == 201 ||
-            statu == 202 ||
-            statu == 207 ||
-            statu == 204 ||
-            statu == 205 ||
-            statu == 206 ||
-            statu == 203 &&
-            statu == 301 ||
-            statu == 302 &&
-            statu == 401
-          ){
+          if (statu == 4) {
             // ไม่สำเร็จ
+            $("#Img1").empty();
+            var img4 = `
+            <div class="col mr-2" >
+            <center>
+              <img src="img/showdata/pick-up.gif" alt="">
+              <h6>รับพัสดุ</h6>
+            </center>
+          </div> 
+          <div class="disn_line" style="margin-bottom: 90px"></div>
 
-          }else if (
-            statu == 101 ||
-            statu == 102 ||
-            statu == 103 && 
-            statu == 201 ||
-            statu == 202 ||
-            statu == 207 ||
-            statu == 204 ||
-            statu == 205 ||
-            statu == 206 ||
-            statu == 203 &&
-            statu == 301 ||
-            statu == 302 &&
-            statu == 501
-          ){
+          <div class="col mr-2" style="width: 25;">
+            <center>
+              <img src="img/showdata/on-progress.gif" alt="">
+              <h6>ถ่ายพัสดุขึ้นรถ</h6>
+            </center>
+          </div>
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2">
+            <center>
+              <img src="img/showdata/on-delivery.gif" alt="">
+              <h6>นำจ่ายพัสดุ</h6>
+            </center>
+          </div>
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2">
+            <center>
+              <img src="img/suggestion/icon_unsuccess.png" alt="" width="125">
+              <h6>นำจ่ายไม่สำเร็จ</h6>
+            </center>
+          </div>`;
+            $("#Img1").append(img4);
+          }
+          if (statu == 5) {
             // สำเร็จ
             $("#Img1").empty();
             var img5 = `
@@ -217,23 +203,197 @@ $(function() {
               <img src="img/suggestion/icon_success.png" alt="" width="125">
               <h6>นำจ่ายสำเร็จ</h6>
             </center>
-          </div>`
-          $("#Img1").append(img5);
-
-          }else {
+          </div>`;
+            $("#Img1").append(img5);
+          } else {
             console.log("NOOOOOOO");
           }
         }
       });
     }
+    if (Search == data2) {
+      // console.log("True");
 
-    if (Search != data1) {
+      $.ajax(settings).done(function(data) {
+        var testdata = data.response.items.EI486458074TH;
+
+        $("#showid").empty();
+        for (var i = 0; i < testdata.length; i++) {
+          var id = testdata[i].barcode;
+        }
+        $("#showid").append(id);
+
+        $("#data1").empty();
+        for (var i = 0; i < testdata.length; i++) {
+          status_dates.push(testdata[i].status_date);
+          var Status_Dates = status_dates[i] + "<br>";
+          $("#data1").append(Status_Dates);
+        }
+
+        $("#data2").empty();
+        for (var i = 0; i < testdata.length; i++) {
+          locations.push(testdata[i].location);
+          var Locations = locations[i] + "<br>";
+          $("#data2").append(Locations);
+        }
+
+        $("#data3").empty();
+        for (var i = 0; i < testdata.length; i++) {
+          status_descriptions.push(testdata[i].status_description);
+          var Status_Descriptions = status_descriptions[i] + "<br>";
+          $("#data3").append(Status_Descriptions);
+        }
+
+        for (var i = 0; i < testdata.length; i++) {
+          var str = testdata[i].status;
+          console.log(str);
+
+          statu = str.charAt(0);
+          console.log("char: " + statu);
+
+          if (statu == 1) {
+            $("#Img1").empty();
+            var img1 = `
+          <div class="col mr-2" >
+            <center>
+              <img src="img/showdata/pick-up.gif" alt="">
+              <h6>รับพัสดุ</h6>
+            </center>
+          </div> 
+          `;
+            $("#Img1").append(img1);
+          }
+          if (statu == 2) {
+            $("#Img1").empty();
+            var img2 = `
+            <div class="col mr-2" >
+            <center>
+              <img src="img/showdata/pick-up.gif" alt="">
+              <h6>รับพัสดุ</h6>
+            </center>
+          </div> 
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2" style="width: 25;">
+            <center>
+              <img src="img/showdata/on-progress.gif" alt="">
+              <h6>ถ่ายพัสดุขึ้นรถ</h6>
+            </center>
+          </div>`;
+            $("#Img1").append(img2);
+          }
+          if (statu == 3) {
+            $("#Img1").empty();
+            var img3 = `
+            <div class="col mr-2" >
+            <center>
+              <img src="img/showdata/pick-up.gif" alt="">
+              <h6>รับพัสดุ</h6>
+            </center>
+          </div>
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2" style="width: 25;">
+            <center>
+              <img src="img/showdata/on-progress.gif" alt="">
+              <h6>ถ่ายพัสดุขึ้นรถ</h6>
+            </center>
+          </div>
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2">
+            <center>
+              <img src="img/showdata/on-delivery.gif" alt="">
+              <h6>นำจ่ายพัสดุ</h6>
+            </center>
+          </div>`;
+            $("#Img1").append(img3);
+          }
+          if (statu == 4) {
+            // ไม่สำเร็จ
+            $("#Img1").empty();
+            var img4 = `
+            <div class="col mr-2" >
+            <center>
+              <img src="img/showdata/pick-up.gif" alt="">
+              <h6>รับพัสดุ</h6>
+            </center>
+          </div> 
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2" style="width: 25;">
+            <center>
+              <img src="img/showdata/on-progress.gif" alt="">
+              <h6>ถ่ายพัสดุขึ้นรถ</h6>
+            </center>
+          </div>
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2">
+            <center>
+              <img src="img/showdata/on-delivery.gif" alt="">
+              <h6>นำจ่ายพัสดุ</h6>
+            </center>
+          </div>
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2">
+            <center>
+              <img src="img/suggestion/icon_unsuccess.png" alt="" width="125">
+              <h6>นำจ่ายไม่สำเร็จ</h6>
+            </center>
+          </div>`;
+            $("#Img1").append(img4);
+          }
+          if (statu == 5) {
+            // สำเร็จ
+            $("#Img1").empty();
+            var img5 = `
+            <div class="col mr-2" >
+            <center>
+              <img src="img/showdata/pick-up.gif" alt="">
+              <h6>รับพัสดุ</h6>
+            </center>
+          </div>
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2" style="width: 25;">
+            <center>
+              <img src="img/showdata/on-progress.gif" alt="">
+              <h6>ถ่ายพัสดุขึ้นรถ</h6>
+            </center>
+          </div>
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2">
+            <center>
+              <img src="img/showdata/on-delivery.gif" alt="">
+              <h6>นำจ่ายพัสดุ</h6>
+            </center>
+          </div>
+          <div class="disn_line" style="margin-bottom: 90px"></div>
+
+          <div class="col mr-2">
+            <center>
+              <img src="img/suggestion/icon_success.png" alt="" width="125">
+              <h6>นำจ่ายสำเร็จ</h6>
+            </center>
+          </div>`;
+            $("#Img1").append(img5);
+          } else {
+            console.log("NOOOOOOO");
+          }
+        }
+      });
+    }
+    if (Search != data1 && Search != data2) {
       Delete_data1.innerHTML = "";
       Delete_data2.innerHTML = "";
       Delete_data3.innerHTML = "";
       Delete_showid.innerHTML = "";
+      Delete_img.innerHTML = "";
+      alert("ไม่มีเลขพัสดุที่ท่านกำลังตามหา");
     }
-
     if (Search == "") {
       alert("กรุณกรอกเลขพัสดุที่คุณต้องการหา");
     }
